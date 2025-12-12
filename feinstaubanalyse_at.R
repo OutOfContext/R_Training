94bc068eae9011bfdb981ca2aa2dfa99b0efb3d6
Raffael Hühnerschulte <huehnerschulte.raffael@gmail.com>
2025-12-12 09:34:01 +0100

    Wir haben "Hello World" zu einer einfachen Kalkulation verändert.

--- feinstaubanalyse_at.R
+++ feinstaubanalyse_at.R
@@ -1,4 +1,17 @@
-install.packages("tidyverse")
 library(tidyverse)
 
-print("hallo welt.")
+gesamt_check = 63
+
+
+sektor1 <- c(1,2,3,4,5,6)
+sektor2 <- sektor1 * 2
+
+gesamt = sum(sektor1) + sum(sektor2) 
+
+gesamt
+
+if(gesamt_check == gesamt){
+  print("Sektor Summe passt.")
+} else {
+  print("Sektor Summe passt nicht.")
+}
